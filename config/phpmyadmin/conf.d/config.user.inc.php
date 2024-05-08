6cb46032fcc0d3e51e9871fe5dfd896a4b6da017
Vu Thanh Thao <thaomaniac@gmail.com>
2024-05-08 17:35:14 +0700

    update phpmyadmin config

---
+++ config/phpmyadmin/conf.d/config.user.inc.php
@@ -0,0 +1,5 @@
+<?php
+$cfg['TitleDatabase'] = '@DATABASE@';
+$cfg['TitleTable'] = '@DATABASE@ / @TABLE@';
+$cfg['TitleServer'] = '@HTTP_HOST@ / @VSERVER@ | @PHPMYADMIN@';
+$cfg['TitleDefault'] = '@HTTP_HOST@ | @PHPMYADMIN@';
