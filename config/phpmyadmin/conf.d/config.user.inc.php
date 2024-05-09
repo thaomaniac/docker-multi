7b745b29f03213e94909a7de7d317f2d79bf608d
Vu Thanh Thao <thaovt@magenest.com>
2024-05-09 08:48:42 +0700

    update phpmyadmin config

--- config/phpmyadmin/conf.d/config.user.inc.php
+++ config/phpmyadmin/conf.d/config.user.inc.php
@@ -3,3 +3,7 @@ $cfg['TitleDatabase'] = '@DATABASE@';
 $cfg['TitleTable'] = '@DATABASE@ / @TABLE@';
 $cfg['TitleServer'] = '@HTTP_HOST@ / @VSERVER@ | @PHPMYADMIN@';
 $cfg['TitleDefault'] = '@HTTP_HOST@ | @PHPMYADMIN@';
+$cfg['LoginCookieValidity'] = 10;
+$sessionValidity = 60 * 60 * 24 * 365 * 1;
+$cfg['LoginCookieValidity'] = $sessionValidity;
+ini_set('session.gc_maxlifetime', (string)$sessionValidity);
