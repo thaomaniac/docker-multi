2940bf0ecebf667ef78a39b653a33cb5bb2b3192
Vu Thanh Thao <thaovt@magenest.com>
2024-07-22 16:56:52 +0700

    master: Update

--- sample/magento/env.php
+++ sample/magento/env.php
@@ -47,29 +47,9 @@ return [
         ]
     ],
     'x-frame-options' => 'SAMEORIGIN',
-    'MAGE_MODE' => 'default',
+    'MAGE_MODE' => 'developer',
     'session' => [
-        'save' => 'redis',
-        'redis' => [
-            'host' => 'redis',
-            'port' => '6379',
-            'password' => '',
-            'timeout' => '2.5',
-            'persistent_identifier' => '',
-            'database' => '3',
-            'compression_threshold' => '2048',
-            'compression_library' => 'gzip',
-            'log_level' => '1',
-            'max_concurrency' => '25',
-            'break_after_frontend' => '5',
-            'break_after_adminhtml' => '30',
-            'first_lifetime' => '600',
-            'bot_first_lifetime' => '60',
-            'bot_lifetime' => '7200',
-            'disable_locking' => '0',
-            'min_lifetime' => '60',
-            'max_lifetime' => '2592000'
-        ]
+        'save' => 'files'
     ],
     'cache' => [
         'frontend' => [
@@ -115,25 +95,9 @@ return [
     'install' => [
         'date' => 'Wed, 22 Sep 2021 06:54:56 +0000'
     ],
-    'modules' => [
-        'Magento_TwoFactorAuth' => 0,
-    ],
-    'MAGE_INDEXER_THREADS_COUNT' => 1,
+    //'MAGE_INDEXER_THREADS_COUNT' => 1,
     'system' => [
         'default' => [
-            'dev' => [
-                'grid' => [
-                    'async_indexing' => 0
-                ],
-            ],
-            'catalog' => [
-                'search' => [
-                    'engine' => 'elasticsearch7',
-                    'elasticsearch7_server_hostname' => 'elasticsearch',
-                    'elasticsearch7_server_port' => 9200,
-                    'elasticsearch7_index_prefix' => 'magento'
-                ]
-            ],
             'web' => [
                 'unsecure' => [
                     'base_url' => 'https://magento.local/',
@@ -153,6 +117,33 @@ return [
                     'cookie_lifetime' => 864000,
                 ]
             ],
+            'catalog' => [
+                'search' => [
+                    'engine' => 'elasticsearch7',
+                    'elasticsearch7_server_hostname' => 'elasticsearch',
+                    'elasticsearch7_server_port' => 9200,
+                    'elasticsearch7_index_prefix' => 'magento'
+                ]
+            ],
+            //'recaptcha_backend' => [
+            //    'type_for' => [
+            //        'user_login' => null,
+            //        'user_forgot_password' => null,
+            //    ]
+            //],
+            //'recaptcha_frontend' => [
+            //    'type_for' => [
+            //        'customer_login' => null,
+            //        'customer_create' => null,
+            //        'customer_forgot_password' => null,
+            //        'customer_edit' => null,
+            //    ]
+            //],
+            //'customer' => [
+            //    'captcha' => [
+            //        'enable' => 0
+            //    ]
+            //],
             'admin' => [
                 'security' => [
                     'use_form_key' => 0,
@@ -165,11 +156,6 @@ return [
                     'enable' => 0
                 ]
             ],
-            'customer' => [
-                'captcha' => [
-                    'enable' => 0
-                ]
-            ],
             'system' => [
                 'smtp' => [
                     'transport' => 'smtp',
@@ -177,6 +163,31 @@ return [
                     'port' => '1025',
                 ]
             ],
-        ]
+            //'dev' => [
+            //    'grid' => [
+            //        'async_indexing' => 0 // default: 0
+            //    ],
+            //],
+        ],
+        //'websites' => [
+        //    'website1' => [
+        //        'web' => [
+        //            'unsecure' => [
+        //                'base_url' => 'https://website1.local/',
+        //                'base_link_url' => '{{unsecure_base_url}}',
+        //                'base_static_url' => null,
+        //                'base_media_url' => null
+        //            ],
+        //            'secure' => [
+        //                'base_url' => '{{unsecure_base_url}}',
+        //                'base_link_url' => '{{secure_base_url}}',
+        //                'base_static_url' => null,
+        //                'base_media_url' => null,
+        //                'use_in_frontend' => 0,
+        //                'use_in_adminhtml' => 0
+        //            ]
+        //        ]
+        //    ]
+        //],
     ]
 ];
