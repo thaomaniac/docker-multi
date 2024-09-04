8cc6efd260ed8c1648f33ed187bf202edd10b605
Vu Thanh Thao <thaomaniac@gmail.com>
2024-09-04 15:17:59 +0700

    master: Update

--- config/phpmyadmin/conf.d/config.user.inc.php
+++ config/phpmyadmin/conf.d/config.user.inc.php
@@ -1,10 +1,13 @@
 <?php
+//https://docs.phpmyadmin.net/en/latest/config.html
 $cfg['TitleDatabase'] = '@DATABASE@ | @PHPMYADMIN@';
 $cfg['TitleTable'] = '@DATABASE@ / @TABLE@ | @PHPMYADMIN@';
 $cfg['TitleServer'] = '@HTTP_HOST@ / @VSERVER@ | @PHPMYADMIN@';
 $cfg['TitleDefault'] = '@HTTP_HOST@ | @PHPMYADMIN@';
 
-$sessionValidity = 60 * 60 * 24 * 365 * 1;
+$cfg['NavigationTreeEnableGrouping'] = false;
+
+$sessionValidity = 60 * 60 * 24 * 365;
 $cfg['LoginCookieValidity'] = $sessionValidity;
 ini_set('session.gc_maxlifetime', (string)$sessionValidity);
 $cfg['ShowPhpInfo'] = true;
