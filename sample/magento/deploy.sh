1512eae1af9aab3ea78aebd4d6fbfb0d137807bd
Vu Thanh Thao <thaovt@magenest.com>
2024-12-16 16:54:19 +0700

    master: Update sample deploy

--- sample/magento/deploy.sh
+++ sample/magento/deploy.sh
@@ -2,36 +2,45 @@
 
 set -e
 
+# Text formatting
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+echo "========== ${bold}Start Deployment...${normal} =========="
+
 DEPLOY_MODE="production"
 
-echo "Pull new code from git:"
+echo "===== Pull new code from git: ====="
 git pull
 
-echo "Enable maintenance mode:"
+echo "===== Enable maintenance mode: ====="
 bin/magento maintenance:enable
 
-echo "Update components with Composer:"
+echo "===== Update with Composer: ====="
 composer update
 
-echo "Set application mode: "
-php8.1 bin/magento deploy:mode:set "${DEPLOY_MODE}" -s
+#echo "To remove generated code & static files:"
+#rm -rf var/cache/*
+#rm -rf var/view_preprocessed/*
+#rm -rf pub/static/*/*
+#rm -rf generated/*/*
 
-echo "To remove generated code & static files:"
-rm -rf var/view_preprocessed/*
-rm -rf pub/static/*/*
-rm -rf generated/*/*
-
-echo "Update the database schema & data:"
+echo "===== Setup Upgrade: ====="
 bin/magento setup:upgrade
 
-echo "Compile the code:"
+echo "===== Set ${DEPLOY_MODE} mode (-s): ====="
+bin/magento deploy:mode:set "${DEPLOY_MODE}" -s
+
+echo "===== Compile the code: ====="
 bin/magento setup:di:compile
 
-echo "Deploy static content:"
+echo "===== Deploy static content: ====="
 bin/magento setup:static-content:deploy
 
-echo "Clean the cache:"
+echo "===== Clean the cache: ====="
 bin/magento cache:clean
 
-echo "Disable maintenance mode:"
-bin/magento maintenance:enable
+echo "===== Disable maintenance mode: ====="
+bin/magento maintenance:disable
+
+echo "========== ${bold}Deployment complete!${normal} =========="
